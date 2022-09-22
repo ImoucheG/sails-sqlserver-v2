846a97d21863830188e6ebb5fe112ce94e6db90a
Imouche Guillaume <g.imouche@atool-dev.fr>
2022-09-22 11:20:56 +0200

    Fix errors on updates

--- helpers/private/utils/get-edits-columns.js
+++ helpers/private/utils/get-edits-columns.js
@@ -3,12 +3,15 @@ module.exports = async function getEditsColumns(statement) {
     let columnsToReturn = [];
     const columns = (statement.insert ? statement.insert : statement.update);
     if (columns && !columns.length) {
-      columnsToReturn = Object.keys(columns).sort();
+      columnsToReturn = Object.keys(columns);
     }
     if (columns.length && columns.length > 0) {
       for (const col of columns) {
         columnsToReturn = columnsToReturn.concat(Object.keys(col).sort());
       }
+    }
+    if (statement.insert) {
+      columnsToReturn = columnsToReturn.sort();
     }
     return Promise.resolve(columnsToReturn);
   } catch (err) {
